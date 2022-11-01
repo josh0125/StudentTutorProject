74f6c73f477d42dba7a09cce1129f3b2c78fe15c
Joshua Allen <joshutallen99@gmail.com>
2022-10-31 21:53:24 -0600

    New Pages

--- profiles/views.py
+++ profiles/views.py
@@ -5,4 +5,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def indexPageView(request) :
-    return HttpResponse('Hello Team!') 
+    return HttpResponse('Home Page') 
+
+def aboutPageView(request) :
+    return HttpResponse('About Page') 
+
+def displayPageView(request) :
+    return HttpResponse('Display Page') 
+
+def inputPageView(request):
+    return HttpResponse('Input Page')
+
+def searchPageView(request):
+    return HttpResponse('Search Page')
