bd8e73bdabe973da585c2ea2335745f450818e7b
Joshua Allen <joshutallen99@gmail.com>
2022-11-14 20:52:07 -0700

    module

--- studentnetwork/settings.py
+++ studentnetwork/settings.py
@@ -1,4 +1,5 @@
 import os
+from getpass import getpass
 
 """
 Django settings for studentnetwork project.
@@ -78,10 +79,13 @@ WSGI_APPLICATION = 'studentnetwork.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'profiles',
+    'USER': 'postgres',
+    'PASSWORD': getpass(),
+    'HOST': 'localhost'
     }
-}
+}   
 
 
 # Password validation
@@ -128,3 +132,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
