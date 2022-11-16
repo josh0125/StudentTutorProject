74d87f77d5082b6237ca63709c9fd4f234aab107
Joshua Allen <joshutallen99@gmail.com>
2022-11-15 17:08:44 -0700

    Create Button isn't working

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,7 +5,9 @@ from .views import aboutPageView
 from .views import displayPageView
 from .views import inputPageView
 from .views import searchPageView
-
+from .views import inputProfilePageView
+from .views import searchProfilePageView
+from .views import findProfilePageView
 
 
 urlpatterns = [
@@ -13,10 +15,18 @@ urlpatterns = [
 
     path("about", aboutPageView, name="about"),   
 
-    path("display", displayPageView, name="display"),  
+    path("display/", displayPageView, name="display"),  
 
     path("input", inputPageView, name="input"), 
 
     path("search", searchPageView, name="search"),
+
+    path("searchprofile/", searchProfilePageView, name="searchprofile"),
+    
+    path("findprofile/", findProfilePageView, name="findprofile"),
+
+    path("inputprofile/", inputProfilePageView, name="inputprofile"),
+
+
 ]   
 
