2b932382cf29947048fcecfdabbf746a1a629783
joshminer3 <josh.miner356@gmail.com>
2022-12-12 11:08:20 -0700

    !!GitHub_Desktop<JoshMiner>

--- studentnetwork/settings.py
+++ studentnetwork/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     'ENGINE': 'django.db.backends.postgresql',
     'NAME': 'profiles',
     'USER': 'postgres',
-    'PASSWORD': 'NEWYORKUT1CAM1SS1ON',
+    'PASSWORD': getpass(),
     'HOST': 'localhost'
     }
 }   
