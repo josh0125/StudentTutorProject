5238b901a76021c5c2528c5a14becb489b5ecf56
Joshua Allen <joshutallen99@gmail.com>
2022-12-12 19:03:16 -0700

    new everthgin

--- studentnetwork/settings.py
+++ studentnetwork/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     'ENGINE': 'django.db.backends.postgresql',
     'NAME': 'profiles',
     'USER': 'postgres',
-    'PASSWORD': '1210',
+    'PASSWORD': 'NEWYORKUT1CAM1SS1ON',
     'HOST': 'localhost'
     }
 }   
